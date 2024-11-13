50a110ffda2c383394171b0630763ebd0bab3fd7
Victoria <vparizot@g.hmc.edu>
2024-11-12 21:08:40 -0800

    syncing my laptop code -vic

--- pariVo_fpga/i2s.sv
+++ pariVo_fpga/i2s.sv
@@ -1,3 +1,51 @@
+// digitalFiltering d1(clk, reset, filterCoefficients, eqLow0, eqHigh0, audioIn, audioOut)
+// Implement FIR (Finite Impulse Response)
+module digitalFiltering(input logic clk, 
+                        input logic reset,
+                        input logic filterCoefficients, // 
+                        input logic [7:0] eqLow, // Corner freq for low pass and high pass filter The four eq upper and lower bounds for channels 0 and 1
+                        input logic [23:0] musicPacketIn, // 24-bit block of the music to process
+                        output logic [23:0] musicPacketOut); 
+    
+// Define internal variables
+    //logic [23:0] filteredOutput;
+    //logic [23:0] audioIn;
+
+    logic [23:0] coeff[0:3] =  {24'd500, 24'd1000, 24'd1000, 24'd500};  // Coefficients for a 5-tap FIR filter
+    logic [23:0] shiftReg[0:3]; // Shift registers to hold prev values WILL THIS UPDATE FOR EACH TIME THIS FUNC IS CALLED
+    logic [3:0] i = 0;
+    logic [3:0] numTaps = 3;
+
+// determine coefficients
+
+// hard code selections
+// make potentiometer a rotary switch
+// matlab simulation to determine TAPS for filter design
+    // miore taps we have between 5 and 10
+    // program multipliers
+    // look at ice40 website
+// on i2c ADC acts slave
+// DSP functional blocks and DSP functional useage guide
+
+
+
+// LOW PASS FILTER w/ FIR
+    always @(posedge clk) begin
+        // Shift register to hold previous samples
+        for (i = numTaps; i > 0; i = i - 1)
+            shift_reg[i] <= shift_reg[i-1];
+        shift_reg[0] <= musicPacketIn;
+
+        // FIR filtering - multiply and accumulate
+        musicPacketOut <= 0;
+        for (i = 0; i <= numTaps; i = i + 1)
+            musicPacketOut <= musicPacketOut + (shift_reg[i] * coeff[i]);
+    end 
+
+
+endmodule
+
+
 module final_fpga(input logic        clk,   //   12MHz MAX1000 clk, H6
                   input logic        nreset, //  global reset,      E6 (right btn)
                   input logic        din, //     I2S DOUT,          PB6_G12
@@ -11,15 +59,14 @@ module final_fpga(input logic        clk,   //   12MHz MAX1000 clk, H6
                   output logic       fmt, //     PCM1808 FMT,       PA8_J10
                   output logic [1:0] md, //      PCM1808 MD1 & MD0, PC7_H13, PA9_H10
                   output logic       miso, //    SPI MISO,          PB4_K11
-                  output logic [7:0] LEDs, //    debug LEDs         (see MAX1000 user guide)
+                  output logic       LEDs, //    debug LEDs         (see MAX1000 user guide)
                   output logic       beat_out // beat (to MCU)      H5 (thru a jumper to MCU)
                   );
 
    ///////////////////////////////// reset
    // active high reset, active low btns
-   logic                             reset;
+   logic  reset;
    assign reset = ~(nreset);
-   //assign LEDs[0] = reset;
    ///////////////////////////////// end reset
 
    ///////////////////////////////// I2S/PCM1808
@@ -31,19 +78,26 @@ module final_fpga(input logic        clk,   //   12MHz MAX1000 clk, H6
    i2s pcm_in(clk, reset, din, bck, lrck, scki, left, right, newsample);
    ///////////////////////////////// end I2S/PCM1808
 
-   ///////////////////////////////// FFT
-   logic                             fft_start, fft_done, i2s_load, fft_load, fft_reset, fft_creset, fft_write;
-   logic [31:0]                      fft_wd;
-   logic signed [15:0]               left_msbs;
-   logic signed [15:0]               fft_rd;
-   logic [5:0]                       sample_ctr;
+
+   ///////////////////////////////// Digital Filtering
+   logic [7:0] eqHigh0, eqLow0, eqHigh1, eqLow1; // The four eq upper and lower bounds for channels 0 and 1
+   logic [23:0] 
+   logic signed [23:0]    
+
+//    logic                             fft_start, fft_done, i2s_load, fft_load, fft_reset, fft_creset, fft_write;
+//    logic [31:0]                      fft_wd;
+//    logic signed [15:0]               left_msbs;
+//    logic signed [15:0]               fft_rd;
+//    logic [5:0]                       sample_ctr;
    
-   assign fft_reset = reset | fft_creset;
-   assign left_msbs = left[23:8];
-   assign fft_rd    = left_msbs >>> 5; // arithmetic shift right
-   fft fft(clk, fft_reset, fft_start, fft_load, fft_rd, fft_wd, fft_done);
-   fft_control i2s_fft_glue(clk, reset, newsample, fft_done, fft_start, fft_load, fft_creset, fft_write, sample_ctr);
-   ///////////////////////////////// end FFT
+//    assign fft_reset = reset | fft_creset;
+//    assign left_msbs = left[23:8];
+//    assign fft_rd    = left_msbs >>> 5; // arithmetic shift right
+//    fft fft(clk, fft_reset, fft_start, fft_load, fft_rd, fft_wd, fft_done);
+//    fft_control i2s_fft_glue(clk, reset, newsample, fft_done, fft_start, fft_load, fft_creset, fft_write, sample_ctr);
+
+
+   ///////////////////////////////// end Digital Filtering
 
    ///////////////////////////////// store result         
    logic [31:0]                      res_count; // result count
@@ -89,57 +143,7 @@ module final_fpga(input logic        clk,   //   12MHz MAX1000 clk, H6
    
 endmodule // final_fpga
 
-typedef enum logic [3:0] {FFT_LOADING, FFT_STARTING, FFT_WAITING, FFT_WRITING, FFT_RESETTING, FFT_ERROR} statetype;
-module fft_control(input logic        clk,
-                   input logic        reset,
-                   input logic        newsample,
-                   input logic        fft_done,
-                   output logic       fft_start,
-                   output logic       fft_load,
-                   output logic       fft_reset,
-                   output logic       fft_write,
-                   output logic [5:0] sample_ctr);
 
-   statetype                   state, nextstate;
-   
-   always_ff @(posedge clk) begin
-      if      (reset || (state == FFT_RESETTING))     
-        sample_ctr <= 0;
-      else if ((newsample && (state == FFT_LOADING)) || (state == FFT_WRITING || nextstate == FFT_WRITING)) 
-        sample_ctr <= sample_ctr + 1'b1;
-      else if (state == FFT_WAITING) 
-        sample_ctr <= 0; // this case needs to be checked after the previous
-      //                    in case state is WAITING but nextstate is WRITING.
-   end
-
-   always_ff @(posedge clk) begin
-      if (reset) state <= FFT_LOADING;
-      else state <= nextstate;
-   end
-
-   always_comb begin
-      case (state)
-        FFT_LOADING  : 
-          if (sample_ctr == 32)   nextstate = FFT_STARTING;
-          else                    nextstate = FFT_LOADING;
-        FFT_STARTING :            nextstate = FFT_WAITING;
-        FFT_WAITING  :            
-          if (fft_done)           nextstate = FFT_WRITING;
-          else                    nextstate = FFT_WAITING;
-        FFT_WRITING  :            
-          if (sample_ctr == 32)   nextstate = FFT_RESETTING;
-          else                    nextstate = FFT_WRITING;
-        FFT_RESETTING:            nextstate = FFT_LOADING;
-        default      :            nextstate = FFT_ERROR; // debug
-      endcase // case (state)
-   end
-
-   assign fft_load  = (state == FFT_LOADING) & newsample;
-   assign fft_start = (state == FFT_STARTING);
-   assign fft_reset = (state == FFT_RESETTING);
-   assign fft_write = (state == FFT_WRITING || nextstate == FFT_WRITING);
-   
-endmodule // control
 
 module i2s(input logic         clk,
            input logic         reset,
---
+++ pariVo_mcu/lib/STM32L432KC_DAC.c
@@ -0,0 +1,96 @@
+/*
+File: STM32L432KC_DAC.c
+Author(s): Victoria Parizot & Audrey Vo
+Email(s): vparizot@g.hmc.edu
+Date: 11/9/24
+
+Source code for DAC functions
+*/
+
+// #include "STM32L432KC_DAC.h"
+// #include "stm32l432xx.h"
+
+/*
+initDAC 
+12 bit resolution, right aligned
+two output channels: DAC1_OUT1 on PA4, DAC1_OUT2 on PA5
+normal mode
+need to set 
+*/
+void initDAC(int resolution) {
+    
+    // Channel 1 Initilization
+    RCC->APB1ENR1 |= RCC_APB1ENR1_DACC1EN; // RCC ENABLE
+    DAC-->CR |= _VAL2FLD(DAC_CR_WAVE1, 0b01); //  DAC channel1 Noise wave generation enabled (pg. 507)
+    DAC-->CR |= _VAL2FLD(DAC_CR_TEN1, 0b1); // DAC channel1 trigger enable
+    DAC-->CR |= _VAL2FLD(DAC_CR_TSEL1, 0b010); // : DAC channel1 trigger selection to TIM7 (pg. 492)
+    DAC-->CR |= _VAL2FLD(DAC_CR_EN1, 0b1); // DAC channel powered on by setting its corresponding ENx bit in the DAC_CR register. (p. 489)
+    DAC-->CR |= _VAL2FLD(DAC_CR_DMAEN1, 0b1); // Enable DMA
+
+
+    // 17.7.1 DAC control register (DAC_CR)
+    // DAC-->CR |= _VAL2FLD(DAC_CR_WAVE2, 0b01); //  DAC channel2 Noise wave generation enabled (pg. 506)
+    // DAC-->CR |= _VAL2FLD(DAC_CR_TEN2, 0b0); //
+    // DAC-->CR |= _VAL2FLD(DAC_CR_EN2, 0b1); // EN2, 1: DAC channel2 enabled
+   
+    
+    // 17.7.2 DAC software trigger register (DAC_SWTRGR)
+    // Do I need to set software triggers?
+
+    // STEP 2: Set 12-bit right alignment: 
+    // data for DAC channel1 to be loaded into the DAC_DHR12RD [11:0] bits (stored into the DHR1[11:0] bits)
+    // data for DAC channel2 to be loaded into the DAC_DHR12RD [27:16] bits (stored into the DHR2[11:0] bits)
+    // The DHR1 and DHR2 registers are then loaded into the DAC_DOR1 and DOR2 registers
+    // Data stored in the DAC_DHRx register are automatically transferred to the DAC_DORx register after one APB1 clock cycle,
+    
+    // 17.7.9 Dual DAC 12-bit right-aligned data holding register (DAC_DHR12RD)
+
+    // 17.7.12 DAC channel1 data output register (DAC_DOR1)
+    // 17.7.13 DAC channel2 data output register (DAC_DOR2)
+
+    //STEP 3: SET MODEx[2:0] in DAC_MCR register to 010 for Normal Mode, Disabled buffer, Connected to external pin (pg. 497)
+        // IF I USE THE BUFFER, THEN CALLIBRATION CLD BE APPLIED (pg. 498)
+
+    //DAC-->MCR |= _VAL2FLD(DAC_MCR_MODE2, 0b010);
+    //DAC-->MCR |= _VAL2FLD(DAC_MCR_MODE1, 0b010);
+    //STEP 4: Dual DAC channel conversion (if available) (pg. 499 - 508)
+}
+
+
+void initDMA() {
+
+	RCC->AHB1ENR |= (1 << 0);   // Enable DMA 1
+
+	DMA1_Channel3->CPAR = (uint32_t) &(DAC1->DHR8R1); // Peripheral address that where we put data into
+
+	DMA1_Channel3->CMAR = (uint32_t) &adcValues;  // Memory address that where we read data from
+
+	DMA1_Channel3->CNDTR = 65460;  // How many data are there ?
+
+	DMA1_Channel3->CCR |= (1 << 4);  // Read from memory
+
+	DMA1_Channel3->CCR |= (1 << 7);  // Memory increment mode
+
+	DMA1_Channel3->CCR &= ~(3 << 8);  // Peripheral size = 8 bits
+
+	// DMA1_Channel3->CCR |= (1 << 5);  // Circular mode
+
+	DMA1_Channel3->CCR &= ~(3 << 10); // Memory size = 8 bits
+
+	DMA1_CSELR->CSELR |= (6 << 8);  // Channel 3 => DAC1_Channel1
+
+	DMA1_Channel3->CCR |= (1 << 0); // Enable channel
+
+}
+// void initChannel(int channel) {
+  
+// }
+
+// uint16_t readADC() {
+ 
+// }
+
+
+
+
+
